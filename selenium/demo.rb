253f51a35322857bf17cfbc950354451ee6ffe1d
Jason Mavandi <mvndaai@gmail.com>
2014-08-13 00:34:27 -0600

    Seleium fixes

--- selenium/demo.rb
+++ selenium/demo.rb
@@ -48,6 +48,7 @@ begin
   sleep 1
   top = driver.find_element(:id, 'top')
   top.location_once_scrolled_into_view
+  sleep 1
 
   driver.manage.window.move_to(300, 400)
   sleep 1
@@ -71,56 +72,5 @@ ensure
   driver.quit
 end
 
-
-
 #https://code.google.com/p/selenium/wiki/RubyBindings
 #http://selenium.googlecode.com/svn/trunk/docs/api/rb/Selenium/WebDriver/ActionBuilder.html
-
-=begin
-# execute arbitrary javascript
-puts driver.execute_script("return window.location.pathname")
-
-# pass elements between Ruby and JavaScript
-element = driver.execute_script("return document.body")
-driver.execute_script("return arguments[0].tagName", element) #=> "BODY"
-
-# wait for a specific element to show up
-wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-wait.until { driver.find_element(:id => "foo") }
-
-# switch to a frame
-driver.switch_to.frame "some-frame" # name or id
-driver.switch_to.frame driver.find_element(:id, 'some-frame') # frame element
-
-# switch back to the main document
-driver.switch_to.default_content
-
-# repositionning and resizing browser window:
-driver.manage.window.move_to(300, 400)
-driver.manage.window.resize_to(500, 800)
-driver.manage.window.maximize
-
-# get an attribute
-class_name = element.attribute("class")
-
-# is the element visible on the page?
-element.displayed?
-
-# click the element
-element.click
-
-# get the element location
-element.location
-
-# scroll the element into view, then return its location
-element.location_once_scrolled_into_view
-
-# get the width and height of an element
-element.size
-
-# press space on an element - see Selenium::WebDriver::Keys for possible values
-element.send_keys :space
-
-# get the text of an element
-element.text
-=end
