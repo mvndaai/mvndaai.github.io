99358b26a2d97b492ca741829adc51e9c8d7b5b3
Jason Mavandi <mvndaai@gmail.com>
2014-08-21 17:01:20 -0600

    local storage notes

--- html5/storage_script.js
+++ html5/storage_script.js
@@ -75,9 +75,26 @@ $( document ).ready(function() {
   	document.cookie = name+"="+value+expires+"; path=/";
   }
 
+  function readCookie(name) {
+  	var nameEQ = name + "=";
+  	var ca = document.cookie.split(';');
+  	for(var i=0;i < ca.length;i++) {
+  		var c = ca[i];
+  		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+  		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  	}
+  	return null;
+  }
+
   function eraseCookie(name) {
     createCookie(name,"",-1);
   }
 
-
+  //if (window["localStorage"])
+  //localStorage.setItem(key,value);
+  //localStorage[key]=value1;
+  //localStorage.getItem(key);
+  //localStorage[key];
+  //localStorage.removeItem(key);
+  //localStorage.clear();
 });
