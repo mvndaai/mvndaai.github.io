9625d878da8c4c5bf75d2eb29da541bdd8cea231
Jason Mavandi <mvndaai@gmail.com>
2014-08-21 17:20:14 -0600

    local storage click item

--- html5/storage_script.js
+++ html5/storage_script.js
@@ -29,7 +29,7 @@ $( document ).ready(function() {
   });
 
   $(".item_local").click(function() {
-    var key = this.html;//.split("=")[0];
+    var key = $(this).html;//.split("=")[0];
     console.log(key);
   });
 
