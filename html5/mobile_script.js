3bc546c0bcb365239068f09a64080221c21439b2
Jason Mavandi <mvndaai@gmail.com>
2015-01-15 20:13:01 -0700

    update mobile

--- html5/mobile_script.js
+++ html5/mobile_script.js
@@ -36,7 +36,7 @@ $( document ).ready(function() {
     }
 
     function selectPageType(){
-        if ((isMobileDevice() && key in localStorage) || !(isMobileDevice())){
+        if ((isMobileDevice() && (key in localStorage)) || !(isMobileDevice()) ){
             return "desktop";
         }else{
             return "mobile";
