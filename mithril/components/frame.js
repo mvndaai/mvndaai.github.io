4a660e3aa93a9d775006f4c72009844eed4d2fc0
Jason Mavandi <jason.mavandi@emc.com>
2017-07-08 21:25:38 -0600

    Fix linkedin url

--- mithril/components/frame.js
+++ mithril/components/frame.js
@@ -37,7 +37,7 @@ let footer = {
     view: _ => m('footer.footer', m('.container', m('.content.has-text-centered', [
         m('.social',[
             m('a[href="https://github.com/mvndaai"][target="_blank"]', m('i.fa.fa-github.[aria-hidden="true"]')),
-            m('a[href="http://www.linkedin.com/pub/jason-mavandi/22/655/b97"][target="_blank"]', m('i.fa.fa-linkedin[aria-hidden="true"]')),
+            m('a[href="https://www.linkedin.com/in/jason-mavandi-b9765522/"][target="_blank"]', m('i.fa.fa-linkedin[aria-hidden="true"]')),
             m('a[href="http://www.facebook.com/mvndaai"][target="_blank"]', m('i.fa.fa-facebook-official[aria-hidden="true"]')),
             m('a[href="https://twitter.com/mvndaai"][target="_blank"]', m('i.fa.fa-twitter[aria-hidden="true"]')),
             m('a[href="https://plus.google.com/u/0/+JasonMavandi"][target="_blank"]', m('i.fa.fa-google-plus[aria-hidden="true"]')),
