55e20ecd9696672c73021c0584fe5a43eaa61ed7
Jason Mavandi <jason.mavandi@emc.com>
2017-07-08 18:57:19 -0600

    Resume pdf

--- mithril/routes/resume.js
+++ mithril/routes/resume.js
@@ -1,6 +1,7 @@
 export default {
     view: _ => m('#resume',[
         // https://stackoverflow.com/questions/17784037/how-to-display-pdf-file-in-html
-        m('embed[src="/jason_mavandi_resume.pdf"]'),
+        // m('embed[src="/jason_mavandi_resume.pdf"]'),
+        m('iframe[src="http://docs.google.com/gview?url=http://mvndaai.github.io/jason_mavandi_resume.pdf&embedded=true"][frameborder="0"]')
     ])
 };
