1d70d8bbbabce0d3539bffa38a5cebfe5bab87e1
Jason Mavandi <jason.mavandi@emc.com>
2017-07-08 20:27:07 -0600

    Home page

--- mithril/routes/home.js
+++ mithril/routes/home.js
@@ -1,3 +1,19 @@
 export default {
-    view: _ => m('', 'This is a home screen')
+    view: _ => m('.columns', [
+        m('.column', [
+            m('h1.title', 'Spotlight'),
+            m('p','These are things that I have recently which I think are notable:'),
+            m('.columns.space', [
+                m('.column', [
+                    m('a.subtitle[href="https://github.com/mvndaai/testrail-promise"][target="_blank"]','Testrail Promise'),
+                    m('p','Post test results to Testrail using NodeJS through promises.'),
+                ]),
+                m('.column', [
+                    m('a.subtitle[href="/unity/space-shooter/"][target="_blank"]','Space Shooter'),
+                    m('p','A game developed in Unity following their tutoria.'),
+                ])
+            ]),
+
+        ]),
+    ])
 };
