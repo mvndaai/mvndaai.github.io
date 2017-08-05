b1121ad59c5fc07e52613f03bbbdf66950f2ceeb
CafeLungo <CafeLungo@users.noreply.github.com>
2017-08-05 07:21:01 -0400

    Some small edits to your bio

--- mithril/routes/bio.js
+++ mithril/routes/bio.js
@@ -2,13 +2,13 @@ export default {
     view: _ => m('#bio.content.columns', [
         m('img.column [src="/images/professional.jpg"][aria-hidden="true"]'),
         m('.column ', m('p', [
-            'My name is Jason Mavandi. My name on the internet is ',
+            'My name is Jason Mavandi. My name on the Internet is ',
             m('strong', 'mvndaai '),
-            'which is the is just my last name with the consonants before the vowels. ',
+            'which is just my last name with the consonants before the vowels. ',
             'I am from southern California, but moved to Utah for school and never left. ',
             'I eventually graduated Cum Laude from Utah Valley University. ',
             'I am happily married. ',
-            'I work as a Software Engineer and in general, love developing. '
+            'I work as a Software Engineer and, in general, love developing. '
         ]))
     ])
 };
