ed0c398f2124f56a3d53804f91872a166bfb4e62
Jason J Mavandi <mvndaai+github@gmail.com>
2018-12-10 10:41:12 -0700

    Add a new joke

--- mithril/routes/jokes/other.js
+++ mithril/routes/jokes/other.js
@@ -3,5 +3,9 @@ export default [
     "type": "pause",
     "before": "Time flies like an arrow",
     'after': 'Fruit files like bananas'
+  },
+  {
+    "type": "oneliner",
+    "line": "I used to be a crastinator but recently I went pro.",
   }
 ];
