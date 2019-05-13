1922fcc62a83c712bcd4f2113d68bf1ab5c39ee4
Jason Mavandi <jmavandi@solutionreach.com>
2019-05-13 15:24:32 -0600

    lighthouse issues

--- mithril/routes/home.js
+++ mithril/routes/home.js
@@ -4,11 +4,11 @@ export default {
             m('h1.title', 'Spotlight'),
             m('.columns', [
                 m('.column', [
-                    m('a.subtitle-link[href="https://github.com/mvndaai/testrail-promise"][target="_blank"]','Testrail Promise'),
+                    m('a.subtitle-link[href="https://github.com/mvndaai/testrail-promise"][target="_blank"][rel="noopener"]','Testrail Promise'),
                     m('p','Post test results to Testrail using NodeJS through promises.'),
                 ]),
                 m('.column', [
-                    m('a..subtitle-link[href="/projects/unity/space-shooter/"][target="_blank"]', 'Space Shooter'),
+                    m('a..subtitle-link[href="/projects/unity/space-shooter/"][target="_blank"][rel="noopener"]', 'Space Shooter'),
                     m('p',[
                         'A game developed in Unity following their ',
                         m('a[href="https://unity3d.com/learn/tutorials/projects/space-shooter-tutorial"]','tutorial'),
@@ -21,10 +21,10 @@ export default {
             m('h1.title', 'Presentations'),
             m('table', [
                 m('tr', [
-                    m('td', m('a[href=https://docs.google.com/presentation/d/1aRk_HT2aSaezLXaBHRjgheH02IacW94UBlejZCcnRWU/edit?usp=sharing][target=_blank]', 'WebAuthn'))
+                    m('td', m('a[href=https://docs.google.com/presentation/d/1aRk_HT2aSaezLXaBHRjgheH02IacW94UBlejZCcnRWU/edit?usp=sharing][target=_blank][rel="noopener"]', 'WebAuthn'))
                 ]),
                 m('tr', [
-                    m('td', m('a[href=http://slides.com/mvndaai/why-use-mithriljs/][target=_blank]', 'MithrilJS'))
+                    m('td', m('a[href=http://slides.com/mvndaai/why-use-mithriljs/][target=_blank][rel="noopener"]', 'MithrilJS'))
                 ])
             ])
         ]),
