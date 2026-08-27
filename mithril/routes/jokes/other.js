74d83698eb5706d202d35a28ecaee55f98586a69
Jason Mavandi <mvndaai@gmail.com>
2026-08-27 11:00:52 -0600

    Update other.js
    Add a new joke

--- mithril/routes/jokes/other.js
+++ mithril/routes/jokes/other.js
@@ -9,6 +9,11 @@ export default [
     "line": "I used to be a crastinator but recently I went pro.",
   },
   {
+    "source": "https://icanhazdadjoke.com/j/218xkq49prc",
+    "type": "oneliner",
+    "line": "I was fired from the keyboard factory yesterday. I wasn't putting in enough shifts.",
+  },
+  {
     "source": "https://icanhazdadjoke.com/j/szXDA59U0wc",
     "type": "oneliner",
     "line": "I went on a date last night with a girl from the zoo. It was great. She's a keeper.",
